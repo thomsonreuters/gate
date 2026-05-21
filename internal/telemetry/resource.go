5a5d8c679f904761f500b87a608fd7cd8f5a0cd2
Shubham Hibare <Shubham.Hibare@thomsonreuters.com>
2026-05-21 20:47:43 +0530

    feat: OTLP integration

---
+++ internal/telemetry/resource.go
@@ -0,0 +1,42 @@
+// Copyright 2026 Thomson Reuters
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package telemetry initializes the OpenTelemetry tracer, meter, and log
+// providers. Init returns a shutdown function and is a no-op when OTel is
+// disabled in configuration.
+package telemetry
+
+import (
+	"github.com/thomsonreuters/gate/internal/config"
+	"github.com/thomsonreuters/gate/internal/constants"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
+)
+
+// newResource builds the OTel resource describing this service.
+func newResource(cfg *config.OTelConfig) (*resource.Resource, error) {
+	serviceName := cfg.ServiceName
+	if serviceName == "" {
+		serviceName = constants.ProgramIdentifier
+	}
+
+	return resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+			semconv.ServiceVersion(constants.Version),
+		),
+	)
+}
